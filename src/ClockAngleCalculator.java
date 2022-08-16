7a4942ab9d1da739f0b8eda7af374cf2603d32ea
Anoman <vedantgandhipersonal@gmail.com>
2022-08-16 13:13:16 +0530

    Added a new prbolem

---
+++ src/ClockAngleCalculator.java
@@ -0,0 +1,40 @@
+/**
+ * This problem was asked by Microsoft.
+ * Given a clock time in hh:mm format, determine, to the nearest degree, the angle between the hour and the minute hands.
+ * Bonus: When, during the course of a day, will the angle be zero?
+ */
+class AngleCalculator
+{
+    public  double calculateAngle(String time)
+    {
+        String[] timeSplit = time.split(":");
+        double angle = 0.0;
+        int baseHourOnClock = Integer.parseInt(timeSplit[0]);
+
+        int minOnClock = Integer.parseInt(timeSplit[1]) / 5;
+        while (minOnClock != baseHourOnClock)
+        {
+            System.out.println("Current minOnClock in hours:"+minOnClock);
+            baseHourOnClock = this.incrementHourCount(baseHourOnClock);
+            angle+=30;
+        }
+        return  angle;
+    }
+
+    /**
+     *
+     * @param currenthour
+     * @return
+     */
+    int incrementHourCount(int currenthour)
+    {
+        return  currenthour+1 > 11 ? 0 : currenthour+1;
+    }
+}
+public class ClockAngleCalculator {
+    public static void main(String[] args) {
+        AngleCalculator calculator = new AngleCalculator();
+        System.out.println("Angle on clock when hour is :"+calculator.calculateAngle("05:15"));
+
+    }
+}
